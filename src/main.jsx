be534f73b2ccde5ea9a9436b56a5a90549df0ac5
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-04 19:49:35 +0500

    feat:Removing route

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
- <React.StrictMode>
-    <BrowserRouter>
+  <React.StrictMode>
       <App />
-    </BrowserRouter>
   </React.StrictMode>
 )
